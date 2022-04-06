212b0808d91b41aeb87b52ee295ddeec9cbe91fd
fliptheweb <itisshortemailaddress@gmail.com>
2022-04-06 19:39:02 +0400

    feat(redirects): implement path-to-regexp

--- test/fixture/spa/redirects.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  { from: '/redirected', to: '/' },
-  { from: '/äßU<', to: '/' },
-  { path: '/many/(.*)', to: '/posts/abcde' },
-  { from: '/mapped/:id', redirect: '/posts/:id' }
-]
